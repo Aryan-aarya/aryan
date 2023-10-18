c2c0e4a5324eeb51e518dbb00d48d438d627a029
Aryan Arya <aryanarya1142003@gmail.com>
2023-10-18 15:26:07 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// component first letter is capital
+// there are two types of components functional base and class base
+import './App.css';
+function App()
+{
+    return(
+     <div>
+     </div>
+    )
+}
+export default App;
