446fd6257e5eff086398fcfdc26378af017e06ca
Aryan Arya <aryanarya1142003@gmail.com>
2023-11-04 01:20:35 +0530

    new

--- src/Arya.js
+++ src/Arya.js
@@ -1,15 +1,37 @@
-import React from 'react';
-function Arya() {
+// import React from 'react';
+// function Arya() {
+//     return(
+//         <form>
+//         <label htmlFor="fname">First name:</label>
+//         <br />
+//         <input type="text" id="fname" name="fname" />
+//         <br />
+//         <label htmlFor="lname">Last name:</label>
+//         <br />
+//         <input type="text" id="lname" name="lname" />
+//         </form>
+//     )
+// }
+// export default Arya;
+import { useState } from "react";
+function Arya(){
     return(
-        <form>
-        <label htmlFor="fname">First name:</label>
-        <br />
-        <input type="text" id="fname" name="fname" />
-        <br />
-        <label htmlFor="lname">Last name:</label>
-        <br />
-        <input type="text" id="lname" name="lname" />
-        </form>
-    )
+        <div>
+        <h1>hi everyone</h1>
+        <MyButton />
+        </div>
+    );
 }
+function MyButton(){
+    const[x,y]= useState(0);
+    function a(){
+        y(x + 1);
+    }
+    return (
+        <button onClick={a}>
+        Clicked {x} times
+        </button>
+    );
+}
+
 export default Arya;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Arya from "./Arya";
-import Comp from "./Comp";
+// import Arya from "./Arya";
+// import Comp from "./Comp";
+import NestedList from "./New";
 
+// to call a variable we use {} in react 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-      <Comp text="Welcome" />
-
-      <App />
-      <Arya />
+      <App /> 
+      <NestedList />
     </div>
   </React.StrictMode>
 );
